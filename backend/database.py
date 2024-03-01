5e129804aaaa632665a64a8a95b0cafe8e67cb07
Arnol Fokam <arnolfokam23@gmail.com>
2024-03-01 22:18:01 +0200

    change app to nextjs

--- backend/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
-
-from backend.config import get_settings
-
-engine = create_engine(get_settings().SQL_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
